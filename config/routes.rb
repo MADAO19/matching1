fb694854862349bbbc399b416690629927c2b8f2
MADAO19 <syun051919@icloud.com>
2020-11-10 21:25:59 +0900

    link設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root to: 'top#index'
   devise_for :users
   resources :homes, only: :index
+  resources :users, only: :show do
+    resources :talks, only: [:index, :show]
+    end
 end
